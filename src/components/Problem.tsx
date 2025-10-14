7701fe4095fc63c61dc9021674dedf5a4490b1a4
arutyyyan <aruzhan.zhunusbekova@nu.edu.kz>
2025-10-14 12:11:46 +0500

    changed the text of the button

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -341,7 +341,7 @@ const Problem = () => {
                     onClick={handleClick}
                     className="bg-white text-primary hover:bg-white/90 font-semibold px-6 py-4 rounded-2xl transition-colors shadow-lg"
                   >
-                    Начать обучение
+                    Начать обучаться ИИ
                   </button>
                 </div>
               </div>
